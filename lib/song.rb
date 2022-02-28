aa3d7a0d4d31208638f6f93448f92e5eb01929f6
samuel <samuelmmanor@gmail.com>
2022-02-28 18:05:42 -0500

    completed assignment

--- lib/song.rb
+++ lib/song.rb
@@ -34,19 +34,44 @@ class Song
       VALUES (?, ?)
     SQL
 
-    # insert the song
     DB[:conn].execute(sql, self.name, self.album)
 
-    # get the song ID from the database and save it to the Ruby instance
     self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
 
-    # return the Ruby instance
     self
   end
 
   def self.create(name:, album:)
     song = Song.new(name: name, album: album)
     song.save
+  end
+
+  def self.new_from_db(row)
+    self.new(id: row[0], name: row[1], album: row[2])
+  end
+
+  def self.all
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
 end
